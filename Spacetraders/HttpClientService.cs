471efb9991a826f933f19e2e8361c27ab4bfffd8
mfroach <65737520+mfroach@users.noreply.github.com>
2025-04-26 15:42:01 +0000

    Update HttpClientService.cs
    Might as well take this token out so bots aren't blasting the API with it

--- Spacetraders/HttpClientService.cs
+++ Spacetraders/HttpClientService.cs
@@ -1,10 +1,11 @@
 using System.Net.Http.Headers;
+using Spacetraders;
 
 public class HttpClientService {
     private readonly HttpClient _client;
 
-    public HttpClientService() {
-        var token = "Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZGVudGlmaWVyIjoiVFdJQ0VCT1VORCIsInZlcnNpb24iOiJ2Mi4zLjAiLCJyZXNldF9kYXRlIjoiMjAyNS0wMy0wMSIsImlhdCI6MTc0MTE5NjYxOSwic3ViIjoiYWdlbnQtdG9rZW4ifQ.y7nEUmnILARHYihPrhP1VrrmIvgwRMY2kVAa0IReQvtH7-MNWPwsIbMXJKONAkK7ztTnlM1HgS2v_nG29FAE5IDDe4_3YjSHTCo1vXZX8IWrel7CxqJimSzpXL1yXuq7lsvueEIbM28Dr5mHXxeejGGfYFKmBMDNr0DLqlVMP2_pJmb8wcnKzMZQvcfgj1wOR-rVV8JwKqeJv1GsWdaQr5MHhqTF1mEP4eu_PO07sREl8-Q_e4AfwXV3x-Edwl99sUTucnl9mV50uwbZEXQ_C-9lh_hPs8U0Yqcyy_kTBIS42QehEm4wuqdybnufJiLKlGylj_IdQzL5NKqlRigzbQ";
+    public HttpClientService(string token) {
+        token = $"Bearer {token}";
         _client = new HttpClient();
         _client.DefaultRequestHeaders.Accept.Clear();
         _client.DefaultRequestHeaders.Add("Authorization", token);
